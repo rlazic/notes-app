7de01eb0835463048c2e0ee89e403ac9f0e7c189
rlazic <118766710+rlazic@users.noreply.github.com>
2023-05-09 16:10:41 +0200

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#note-form');
+const noteList = document.querySelector('#note-list');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector('#note-title').value;
+  const text = document.querySelector('#note-text').value;
+
+  if (title === '' || text === '') {
+    alert('Please fill in both the title and text fields');
+    return;
+  }
+
+  const note = document.createElement('div');
+  note.classList.add('note');
+  const noteHTML = `
+    <h2>${title}</h2>
+    <p>${text}</p>
+  `;
+  note.innerHTML = noteHTML;
+  noteList.appendChild(note);
+
+  document.querySelector('#note-title').value = '';
+  document.querySelector('#note-text').value = '';
+});
